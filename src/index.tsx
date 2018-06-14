a42c29406dae97aeef9300acc172274a8ec83782
Danilo Barros <danilobjr@gmail.com>
2018-06-14 00:55:35 -0300

    todos

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { App } from 'components';
+import 'styles';
 
-// console.warn('TODO', 'split CSS code');
-// console.warn('TODO', 'minify CSS in production');
-console.warn('TODO', '"optimization.splitChunks.name: false" does not render chunk files');
-// console.warn('TODO', 'favicon not working');
+console.log('TODO', 'set theme colors');
+console.log('TODO', 'split CSS code');
+console.log('TODO', 'minify CSS in production');
+console.log('TODO', '"optimization.splitChunks.name: false" does not render chunk files');
 
 render(<App />, document.querySelector('#root'));
