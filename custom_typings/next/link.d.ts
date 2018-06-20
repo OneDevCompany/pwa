8b85e82dfea0514b088b2c06b12ac4ec22532d27
Danilo Barros <danilobjr@gmail.com>
2018-06-20 10:26:03 -0300

    sort props in LinkProps

--- custom_typings/next/link.d.ts
+++ custom_typings/next/link.d.ts
@@ -2,16 +2,17 @@ import * as url from "url";
 import * as React from "react";
 
 export type UrlLike = url.UrlObject | url.Url;
-export interface LinkState {
-    prefetch?: boolean;
-    shallow?: boolean;
-    scroll?: boolean;
-    replace?: boolean;
-    onError?(error: any): void;
-    href?: string | UrlLike;
-    as?: string | UrlLike;
-    passHref?: boolean;
-    children: React.ReactElement<any>;
+
+export interface LinkProps {
+  as?: string | UrlLike;
+  children: React.ReactNode;
+  href?: string | UrlLike;
+  passHref?: boolean;
+  prefetch?: boolean;
+  replace?: boolean;
+  scroll?: boolean;
+  shallow?: boolean;
+  onError?: (error: any) => void;
 }
 
-export default class extends React.Component<LinkState> {}
+export default class extends React.Component<LinkProps> { }
