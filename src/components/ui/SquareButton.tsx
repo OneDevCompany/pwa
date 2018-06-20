d2a3ba1a9ab7dd80f72b9fd4aaa0d092c7721f96
Danilo Barros <danilobjr@gmail.com>
2018-06-20 20:27:41 -0300

    pass Ripple props

--- src/components/ui/SquareButton.tsx
+++ src/components/ui/SquareButton.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import { HTMLProps, ReactNode, SFC } from 'react';
 import { Icon } from 'rmwc/Icon';
-import { Ripple } from 'rmwc/Ripple';
+import { Ripple, RippleProps } from 'rmwc/Ripple';
 
-type SquareButtonProps = { use: ReactNode } & HTMLProps<HTMLDivElement>;
+type SquareButtonProps = { use: ReactNode } & RippleProps & HTMLProps<HTMLDivElement>;
 
-export const SquareButton: SFC<SquareButtonProps> = ({ className, use, ...otherProps }) => (
-  <Ripple>
-    <div className={`odc-square-button ${className || ''}`} {...otherProps}>
-      <Icon use={use} />
-    </div>
-  </Ripple>
-);
+export const SquareButton: SFC<SquareButtonProps> = ({
+  accent,
+  className,
+  disabled,
+  primary,
+  unbounded,
+  use,
+  ...otherProps }) => {
+
+  const rippleProps = { accent, disabled, primary, unbounded };
+
+  return (
+    <Ripple {...rippleProps}>
+      <div className={`odc-square-button ${className || ''}`} {...otherProps}>
+        <Icon use={use} />
+      </div>
+    </Ripple>
+  );
+};
