cfcb0f1f622d215ba91b6e83f69cf21688394036
Danilo Barros <danilobjr@gmail.com>
2018-06-20 15:23:35 -0300

    new todo

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ console.warn('TODO', 'set theme colors');
 console.warn('TODO', 'split CSS code');
 console.warn('TODO', 'set a .odc-layout in Layout and put its custom styles there (remove specific from _top-app-bar and _drawer)');
 console.warn('TODO', 'check performance at components');
+console.warn('TODO', 'use ripple main color on SquareButton?');
 console.warn('TODO', 'create a issue at jamesmfriedman/rmwc: TopAppBar:fixed not working. TopAppBar is by default fixed. Set it to fixed={false} doesnt work');
 
 export default () => <h1>Index Page</h1>;
