3fd3c4a98fc942ad4b80cfce3994d9f25c04982e
Danilo Barros <danilobjr@gmail.com>
2018-06-20 15:14:29 -0300

    new todo: performance

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import '../src/styles/index.scss';
 console.warn('TODO', 'set theme colors');
 console.warn('TODO', 'split CSS code');
 console.warn('TODO', 'set a .odc-layout in Layout and put its custom styles there (remove specific from _top-app-bar and _drawer)');
-console.warn('TODO', 'create a _square-button.scss and remove styles from _top-app-bar');
+console.warn('TODO', 'check performance at components');
+console.warn('TODO', 'create a issue at jamesmfriedman/rmwc: TopAppBar:fixed not working. TopAppBar is by default fixed. Set it to fixed={false} doesnt work');
 
 export default () => <h1>Index Page</h1>;
