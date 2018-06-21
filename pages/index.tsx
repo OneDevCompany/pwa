bde6551c0589079be204ab0bb46ddc140dcd7126
Danilo Barros <danilobjr@gmail.com>
2018-06-21 13:40:01 -0300

    remove done todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,11 @@ import { Datagrid } from 'components/ui';
 
 import '../src/styles/index.scss';
 
-console.warn('TODO', 'set theme colors');
 console.warn('TODO', 'split CSS code');
 console.warn('TODO', 'check performance at components');
-console.warn('TODO', 'use ripple main color on SquareButton?');
-console.warn('TODO', 'set a sass variable for height and width of SquareButton and height of Toolbar (Row min-height) and AppTopBar');
-console.warn('TODO', 'set a sass variable for border-bottom of Toolbar and AppTopBar');
-console.warn('TODO', 'create custom Toolbar with specific height and color. Remove common style from drawer toolbar in _drawer.scss');
+console.warn('TODO', 'set font-weight: 300 as default for some typographies');
+console.warn('TODO', 'set default class like .odc-square-button__left-side with only right border');
+console.warn('TODO', 'remove ripple effect from add button in datagrid');
 console.warn('TODO', 'create a issue at jamesmfriedman/rmwc: TopAppBar:fixed not working. TopAppBar is by default fixed. Set it to fixed={false} doesnt work');
 
 export default () => (
