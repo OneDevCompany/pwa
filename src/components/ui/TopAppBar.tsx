136e23549ffcc8c240f2d1e8dfbcfcd3d90d2f9d
Danilo Barros <danilobjr@gmail.com>
2018-06-21 12:52:20 -0300

    refactor for use classnames lib

--- src/components/ui/TopAppBar.tsx
+++ src/components/ui/TopAppBar.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import { SFC } from 'react';
 import { TopAppBar as RmwcTopAppBar, TopAppBarProps } from 'rmwc/TopAppBar';
 
 export const TopAppBar: SFC<TopAppBarProps> = ({ children, className, ...otherProps }) => {
   return (
     <RmwcTopAppBar
-      className={`odc-top-app-bar ${className || ''}`}
+      className={classNames('odc-top-app-bar', className)}
       short
       {...otherProps as any}
     >
