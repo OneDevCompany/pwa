63aaa9910cd079362722341594de45524ec9e1d1
Danilo Barros <danilobjr@gmail.com>
2018-06-21 12:48:47 -0300

    refactor for use classnames lib

--- src/components/ui/TopAppBarNavigationButton.tsx
+++ src/components/ui/TopAppBarNavigationButton.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import classnames from 'classnames';
 import { HTMLProps, SFC } from 'react';
 import { SquareButton } from 'components/ui';
 
 export const TopAppBarNavigationButton: SFC<HTMLProps<HTMLDivElement>> = ({ className, ...otherProps }) => (
   <SquareButton
-    className={`navigation-button ${className || ''}`}
+    className={classnames('navigation-button', className)}
     ripplePrimary
     use="menu"
     {...otherProps}
