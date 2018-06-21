2e0a78ad661c4bc49897f3aa24e33cbe869a04fa
Danilo Barros <danilobjr@gmail.com>
2018-06-21 12:24:34 -0300

    new todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Datagrid } from 'components/ui';
 
 import '../src/styles/index.scss';
 
@@ -6,6 +7,11 @@ console.warn('TODO', 'set theme colors');
 console.warn('TODO', 'split CSS code');
 console.warn('TODO', 'check performance at components');
 console.warn('TODO', 'use ripple main color on SquareButton?');
+console.warn('TODO', 'set a sass variable for height and width of SquareButton and height of Toolbar (Row min-height) and AppTopBar');
+console.warn('TODO', 'set a sass variable for border-bottom of Toolbar and AppTopBar');
+console.warn('TODO', 'create custom Toolbar with specific height and color. Remove common style from drawer toolbar in _drawer.scss');
 console.warn('TODO', 'create a issue at jamesmfriedman/rmwc: TopAppBar:fixed not working. TopAppBar is by default fixed. Set it to fixed={false} doesnt work');
 
-export default () => <h1>Index Page</h1>;
+export default () => (
+  <Datagrid />
+);
