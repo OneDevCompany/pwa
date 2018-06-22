3ddb43bb3c3dca5c07eb512e5eef9692985ad84f
Danilo Barros <danilobjr@gmail.com>
2018-06-22 12:31:18 -0300

    new todo

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ console.warn('TODO', 'set font-weight: 300 as default for some typographies');
 console.warn('TODO', 'set default class like .odc-square-button__left-side with only right border');
 console.warn('TODO', 'remove ripple effect from add button in datagrid');
 console.warn('TODO', 'remove style prop from inner Scrollbars of Datagrid');
+console.warn('TODO', 'add tooltips in buttons and table head');
 console.warn('TODO', 'create a issue at jamesmfriedman/rmwc: TopAppBar:fixed not working. TopAppBar is by default fixed. Set it to fixed={false} doesnt work');
 
 export default () => (
