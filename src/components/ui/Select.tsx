bcf9e4531fc0b476a20f80930097f81efc404a70
Danilo Barros <danilobjr@gmail.com>
2018-06-22 16:44:33 -0300

    create a prototype of Select component

---
+++ src/components/ui/Select.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { Button, ButtonIcon } from 'rmwc/Button';
+import { Menu, MenuAnchor, MenuItem } from 'rmwc/Menu';
+import { Typography } from 'rmwc/Typography';
+
+type SelectProps = {
+  options: string[];
+};
+
+type SelectState = {
+  open: boolean;
+};
+
+export class Select extends Component<SelectProps, SelectState> {
+  constructor(props: SelectProps) {
+    super(props);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <MenuAnchor className="odc-select">
+        <Menu
+          open={open}
+          onClose={this.close}
+        >
+          {this.renderOptions()}
+        </Menu>
+
+        <Button dense onClick={this.open}>
+          <Typography use="caption">5</Typography>
+          <ButtonIcon use="arrow_drop_down" />
+        </Button>
+      </MenuAnchor>
+    );
+  }
+
+  private renderOptions = () => this.props.options.map(o => (
+    <MenuItem key={o}>{o}</MenuItem>
+  ));
+
+  close = () => this.toggleMenu(false);
+
+  open = () => this.toggleMenu(true);
+
+  private toggleMenu = (open: boolean) => this.setState({ open });
+}
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export * from './Datagrid';
 export * from './Layout';
+export * from './Select';
 export * from './SquareButton';
 export * from './TopAppBar';
 export * from './TopAppBarNavigationButton';
