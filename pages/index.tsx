5bc56915d91773e5578c36bcd3af45bcdb67d899
Danilo Barros <danilobjr@gmail.com>
2018-06-25 14:23:53 -0300

    new todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,8 @@ console.warn('TODO', 'remove style prop from inner Scrollbars of Datagrid');
 console.warn('TODO', 'add tooltips in buttons and table head');
 console.warn('TODO', 'refactor css for use BEM');
 console.warn('TODO', 'set :active style for primary buttons (SquareButton)');
+console.warn('TODO', 'fix font-size of filters header');
+console.warn('TODO', 'pagination: decrease padding in items of "Rows per page" <Select>');
 console.warn('TODO', 'create type for "value" and "options" props in Select component');
 console.warn('TODO', 'vertical scrollbars not appear when mobile vision');
 console.warn('TODO', 'set all IconButtons and Buttons as "user-select: none" in css');
