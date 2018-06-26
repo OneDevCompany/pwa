db1c6f9b66aadfc029aad43da52fcc8e7bda8c53
Danilo Barros <danilobjr@gmail.com>
2018-06-26 17:31:38 -0300

    todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,13 @@ import { Datagrid } from 'components/ui';
 
 import '../src/styles/index.scss';
 
-console.warn('TODO', 'split CSS code');
-console.warn('TODO', 'check performance at components');
-console.warn('TODO', 'set font-weight: 300 as default for some typographies');
-console.warn('TODO', 'set default class like .odc-square-button__left-side with only right border');
-console.warn('TODO', 'remove ripple effect from add button in datagrid');
-console.warn('TODO', 'remove style prop from inner Scrollbars of Datagrid');
-console.warn('TODO', 'add tooltips in buttons and table head');
-console.warn('TODO', 'set :active style for primary buttons (SquareButton)');
-console.warn('TODO', 'fix font-size of filters header');
 console.warn('TODO', 'pagination: decrease padding in items of "Rows per page" <Select>');
 console.warn('TODO', 'create type for "value" and "options" props in Select component');
-console.warn('TODO', 'vertical scrollbars not appear when mobile vision');
-console.warn('TODO', 'set all IconButtons and Buttons as "user-select: none" in css');
-console.warn('TODO', 'extract all components with .odc-* class');
-console.warn('TODO', 'use whenever possible .mdc-* classes instead of bring new .odc-* ones');
+console.warn('BUG', 'last column is partially hidden when mobile size');
+console.warn('TODO', 'add tooltips in buttons and table head');
+console.warn('TODO', 'set font-weight: 300 as default for some typographies');
+console.warn('TODO', 'split CSS code');
+console.warn('TODO', 'check performance at components');
 console.warn('TODO', 'create a issue at jamesmfriedman/rmwc: TopAppBar:fixed not working. TopAppBar is by default fixed. Set it to fixed={false} doesnt work');
 
 export default () => (
