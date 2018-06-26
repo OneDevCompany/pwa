d534a746bccb5687e6951cb73bb00c8c5547446f
Danilo Barros <danilobjr@gmail.com>
2018-06-26 17:44:01 -0300

    create DatagridPagination component

--- src/components/ui/Datagrid/Datagrid.tsx
+++ src/components/ui/Datagrid/Datagrid.tsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { Component } from 'react';
 import { Scrollbars } from 'tt-react-custom-scrollbars';
-import { Select, SquareButton } from 'components/ui';
+import { SquareButton } from 'components/ui';
 import { Drawer } from 'rmwc/Drawer';
 import { Icon } from 'rmwc/Icon';
-import { Typography } from 'rmwc/Typography';
 import {
   Toolbar,
   ToolbarIcon,
@@ -13,6 +12,7 @@ import {
   ToolbarSection,
   ToolbarTitle,
 } from 'rmwc/Toolbar';
+import { DatagridPagination } from './DatagridPagination';
 
 type DatagridState = {
   filtersDrawerOpen: boolean;
@@ -270,29 +270,7 @@ export class Datagrid extends Component<{}, DatagridState> {
           </Drawer>
         </div>
 
-        <Toolbar className="odc-pagination">
-          <ToolbarRow>
-            <ToolbarSection alignEnd>
-              <div className="odc-pagination__rows">
-                <Typography use="caption">Rows per page:</Typography>
-                <Select options={['5', '10', '25']} />
-              </div>
-
-              <div className="odc-pagination__counter">
-                <Typography className="odc-pagination__from" use="caption">1</Typography>
-                <Typography use="caption">{' - '}</Typography>
-                <Typography className="odc-pagination__to" use="caption">15</Typography>
-                <Typography use="caption">{' of '}</Typography>
-                <Typography className="odc-pagination__total" use="caption">25</Typography>
-              </div>
-
-              <div className="odc-pagination__actions">
-                <ToolbarIcon use="chevron_left" />
-                <ToolbarIcon use="chevron_right" />
-              </div>
-            </ToolbarSection>
-          </ToolbarRow>
-        </Toolbar>
+        <DatagridPagination />
       </div>
     );
   }
