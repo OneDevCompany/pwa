af2284fe5da1dc1b5884c4378ec55c4d6188ab4d
Danilo Barros <danilobjr@gmail.com>
2018-06-28 17:55:25 -0300

    remove Link custom component

--- src/components/commons/DrawerContent.tsx
+++ src/components/commons/DrawerContent.tsx
@@ -1,21 +1,31 @@
 import * as React from 'react';
+import Link from 'next/link';
 import { Fragment, SFC } from 'react';
-import { Link } from 'components/commons';
 import { ListItem, ListItemText } from 'rmwc/List';
+import { withRouter, WithRouterProps } from 'next/router';
 
 const links = [
   { href: '/', text: 'Home' },
   { href: '/other', text: 'Other' },
 ];
 
-export const DrawerContent: SFC<{}> = () => (
+const DrawerContentComponent: SFC<WithRouterProps> = ({ router }) => (
   <Fragment>
     {links.map(({ href, text }) => (
       <Link key={href} href={href}>
-        <ListItem theme="on-secondary">
+        <ListItem
+          activated={isActive(href, router.pathname)}
+          theme="on-secondary"
+        >
           <ListItemText>{text}</ListItemText>
         </ListItem>
       </Link>
     ))}
   </Fragment>
 );
+
+function isActive(href: string, currentRoute: string) {
+  return currentRoute === href;
+}
+
+export const DrawerContent: SFC<{}> = withRouter(DrawerContentComponent) as any;
--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -1,2 +1,1 @@
 export * from './DrawerContent';
-export * from './Link';
