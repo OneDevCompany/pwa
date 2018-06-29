93dc9ee702ace0274cbe05a2cac77b0d705a50ca
Danilo Barros <danilobjr@gmail.com>
2018-06-29 15:06:10 -0300

    refactor DatagridTable component

--- src/components/ui/Datagrid/DatagridTable.tsx
+++ src/components/ui/Datagrid/DatagridTable.tsx
@@ -12,33 +12,34 @@ import { DatagridTableData } from './models';
 
 type DatagridTableProps = DatagridTableData;
 
-export const DatagridTable: SFC<DatagridTableProps> = ({
-  items,
-  uniqueKey,
-}) => (
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell head>Name</TableCell>
-        <TableCell head sortOrder="asc" active>Email</TableCell>
-        <TableCell head>Profession</TableCell>
-        <TableCell head>Age</TableCell>
-      </TableRow>
-    </TableHead>
+export const DatagridTable: SFC<DatagridTableProps> = (props) => (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell head>Name</TableCell>
+          <TableCell head sortOrder="asc" active>Email</TableCell>
+          <TableCell head>Profession</TableCell>
+          <TableCell head>Age</TableCell>
+        </TableRow>
+      </TableHead>
 
-    <Scrollbars
-      className="odc-table__body-scrollbars"
-      style={{ minWidth: 800 - 100 }}
-    >
-      <TableBody>
-        {items.map(item => {
-          const cells = Object
-            .keys(item)
-            .map(key => <TableCell key={key}>{item[key]}</TableCell>);
+      <Scrollbars
+        className="odc-table__body-scrollbars"
+        style={{ minWidth: 800 - 100 }}
+      >
+        <TableBody>
+          {renderRows(props)}
+        </TableBody>
+      </Scrollbars>
+    </Table>
+  );
 
-          return <TableRow key={item[uniqueKey]}>{cells}</TableRow>;
-        })}
-      </TableBody>
-    </Scrollbars>
-  </Table>
-);
+function renderRows({ items, uniqueKey }: DatagridTableProps) {
+  return items.map(item => {
+    const cells = Object
+      .keys(item)
+      .map(key => <TableCell key={key}>{item[key]}</TableCell>);
+
+    return <TableRow key={item[uniqueKey]}>{cells}</TableRow>;
+  });
+}
