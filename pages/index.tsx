3147ba5c26639cb477a069c6b19e31977b9c2aec
Danilo Barros <danilobjr@gmail.com>
2018-07-02 16:36:31 -0300

    move fake data to a specific file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,7 @@
 import * as React from 'react';
 import Link from 'next/link';
 import { Datagrid, SquareButton } from 'components/ui';
-
-type Person = {
-  name: string;
-  email: string;
-  occupation: string;
-  age: number;
-};
-
-const people = [
-  {
-    name: 'Danilo',
-    email: 'danilo@email.com',
-    occupation: 'Javascript Developer',
-    age: 33,
-  },
-] as Person[];
+import { people } from '../src/pages/indexData';
 
 export default () => (
   <Datagrid
---
+++ src/pages/indexData.tsx
@@ -0,0 +1,159 @@
+type Person = {
+  name: string;
+  email: string;
+  occupation: string;
+  age: number;
+};
+
+export const people = [
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+  {
+    name: 'Danilo',
+    email: 'danilo@email.com',
+    occupation: 'Javascript Developer',
+    age: 33,
+  },
+] as Person[];
